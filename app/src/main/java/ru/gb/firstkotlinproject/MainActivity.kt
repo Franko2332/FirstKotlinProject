cbca28c556b6e96c7e6bb0bcb42917c4e7d33af3
Franko777 <khubul.george@inbox.ru>
2022-02-04 18:27:33 +0300

    ex.4 add onClickListener for button

--- app/src/main/java/ru/gb/firstkotlinproject/MainActivity.kt
+++ app/src/main/java/ru/gb/firstkotlinproject/MainActivity.kt
@@ -1,11 +1,16 @@
 package ru.gb.firstkotlinproject
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var button: Button = findViewById(R.id.button)
+        var textView: TextView = findViewById(R.id.text_view)
+        button.setOnClickListener { textView.text = "hello" }
     }
 }
