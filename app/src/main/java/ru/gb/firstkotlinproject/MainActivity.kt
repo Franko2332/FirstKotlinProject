bc8603dc5d5d5441cd8a06bab9d5caedbd10a65b
Franko777 <khubul.george@inbox.ru>
2022-02-05 22:47:28 +0300

    ex. 5 (a) create data class and print his properties in app (b) copy class

--- app/src/main/java/ru/gb/firstkotlinproject/MainActivity.kt
+++ app/src/main/java/ru/gb/firstkotlinproject/MainActivity.kt
@@ -9,8 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var button: Button = findViewById(R.id.button)
-        var textView: TextView = findViewById(R.id.text_view)
-        button.setOnClickListener { textView.text = "hello" }
+        val button: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.text_view)
+        val people: DataTestClass = DataTestClass("Ivan", 22)
+        button.setOnClickListener { textView.text = people.name+" "+people.age}
+        val anotherPeople = people.copy()
+        val textView2: TextView = findViewById(R.id.text_view2)
+        textView2.text = anotherPeople.name + " " + anotherPeople.age
     }
 }
